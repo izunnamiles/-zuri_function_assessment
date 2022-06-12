26d915c15e042ebcb82502d847f094520503e12a
Izuchukwu Okafor <49151626+izunnamiles@users.noreply.github.com>
2022-06-12 12:26:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const getPlayerName = () => {
+ let name = prompt("Please enter your name", "");
+  const userDetails = {
+    name: name,
+    points: 0,
+    level: 1
+  }
+  range1 = 1;
+  range2 = 2;
+  let guessedNumber = prompt("kindly guess a number", "");
+  return getNumber(range1,range2,guessedNumber,userDetails)
+}
+
+const getNumber = (range1,range2,userInput,userDetails)=>{
+  userLevel = userDetails.level
+  range2 = userLevel > 1 ? userLevel-- : range2
+  let rand = randomNumber(range1,range2)
+  if(userInput == rand){
+    userDetails.points = userDetails.points + 1;
+    userDetails.level = userDetails.level + 1;
+    console.log(`Correct! ${userDetails.name}, you are now in stage ${userDetails.level}`);
+  }else{
+    console.log(`Incorrect guess ${userDetails.name}, you are still in stage ${userDetails.level}`);
+  }
+}
+
+const randomNumber = (range1,range2) => {
+  rand = Math.random() * (range2 - range1 + 1) + range1;
+  return Math.floor(rand)                        
+}
+
+getPlayerName();
